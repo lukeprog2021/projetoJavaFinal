35b9e42e2eda8e24debc28ac522aa49833c37399
Lucas Souza Silva <82843331+lukeprog2021@users.noreply.github.com>
2022-09-26 23:17:19 -0300

    Correção bug Apagar table v1.1

--- src/main/java/view/CadastroCarro.java
+++ src/main/java/view/CadastroCarro.java
@@ -210,23 +210,23 @@ public class CadastroCarro extends javax.swing.JFrame {
         PlacajTextField3.setText("");
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
                 new Object [][] {
-                        {null, null, null, null},
-                        {null, null, null, null},
-                        {null, null, null, null},
-                        {null, null, null, null},
-                        {null, null, null, null},
-                        {null, null, null, null},
-                        {null, null, null, null},
-                        {null, null, null, null},
-                        {null, null, null, null},
-                        {null, null, null, null}
+                        {null, null, null},
+                        {null, null, null},
+                        {null, null, null},
+                        {null, null, null},
+                        {null, null, null},
+                        {null, null, null},
+                        {null, null, null},
+                        {null, null, null},
+                        {null, null, null},
+                        {null, null, null}
                 },
                 new String [] {
-                        "ID", "Modelo", "Marca", "Placa"
+                        "Modelo", "Marca", "Placa"
                 }
         ) {
             Class[] types = new Class [] {
-                    java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+                   java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
 
             public Class getColumnClass(int columnIndex) {
